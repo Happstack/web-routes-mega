4afce6c2fe51bcbd914b0223e06189b9bb13d5b3
Jeremy Shaw <jeremy@seereason.com>
2010-03-18 18:11:23 +0000

    added URLT/Transformers.hs

---
+++ URLT/Transformers.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances, TypeFamilies, PackageImports #-}
+module URLT.Transformers where
+
+import Control.Applicative
+import Control.Monad (MonadPlus)
+import Control.Monad.Fix (MonadFix)
+import "transformers" Control.Monad.Trans (MonadTrans(lift), MonadIO(liftIO))
+import HSX.XMLGenerator (XMLGenT(..))
+import URLT.Base
+
+instance MonadTrans (URLT url) where
+  lift = liftURLT
+  
+instance (MonadIO m) => MonadIO (URLT url m) where  
+  liftIO = lift . liftIO
