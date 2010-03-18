0dfd64074bb183164404fe8179607347cb220ede
Jeremy Shaw <jeremy@seereason.com>
2010-03-18 18:08:37 +0000

    added handleWaiD

--- URLT/Dispatch.hs
+++ URLT/Dispatch.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
 module URLT.Dispatch where
 
 import Network.Wai
+import URLT.Wai
+import URLT.TH
 
 class Dispatch a where
   type Routes a
   type App a
   dispatch :: a -> (Routes a -> String) -> Routes a -> (App a)
 
+handleWaiD :: (Dispatch a, AsURL (Routes a), App a ~ Application) => a -> String -> Application
+handleWaiD args approot = handleWaiU (dispatch args) approot
