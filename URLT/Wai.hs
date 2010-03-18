3ac2be26b123d7ee70b3c320fe2ca5f14228f52c
Jeremy Shaw <jeremy@seereason.com>
2010-03-18 18:09:02 +0000

    added handleWaiU

--- URLT/Wai.hs
+++ URLT/Wai.hs
@@ -5,11 +5,16 @@ import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy.Char8 as L
 import Network.Wai
 import Network.Wai.Enumerator
+import URLT.TH
 
-handleWai :: (url -> String) -> (String -> Failing url) -> ((url -> String) -> url -> Application) -> Application
-handleWai fromUrl toUrl handler =
+handleWai :: (url -> String) -> (String -> Failing url) -> ((url -> String) -> url -> Application) -> String -> Application
+handleWai fromUrl toUrl handler approot =
   \request ->
      do let fUrl = toUrl $ S.unpack $ pathInfo request
         case fUrl of
           (Failure errs) -> return $ Response Status404 [] $ Right $ fromLBS (L.pack $ unlines errs)
-          (Success url) -> handler fromUrl url request
+          (Success url) -> handler (showString approot . fromUrl) url request
+
+handleWaiU :: (AsURL url) => ((url -> String) -> url -> Application) -> String -> Application
+handleWaiU handler approot = handleWai toURL fromURL handler approot
+
