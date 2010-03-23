89eca547562f0173797c95d08407460e9bc17608
Jeremy Shaw <jeremy@seereason.com>
2010-03-23 19:54:14 +0000

    changed fromPathSegments so that it uses the Consumer monad

--- URLT/PathInfo.hs
+++ URLT/PathInfo.hs
@@ -1,20 +1,22 @@
 {-# LANGUAGE FlexibleInstances #-}
 module URLT.PathInfo where
 
+import Control.Applicative
 import Control.Applicative.Error (Failing(Failure, Success))
-import Control.Monad.Consumer (Consumer, next, runConsumer)
+import Control.Monad.Consumer (Consumer(Consumer), runConsumer)
 import URLT.Base
 
 class PathInfo a where
   toPathSegments :: a -> [String]
-  fromPathSegments :: [String] -> Failing (a, [String])
-  
+  fromPathSegments :: Consumer String (Failing a)
+
 instance PathInfo [String] where  
   toPathSegments = id
-  fromPathSegments = Success
-  
+  fromPathSegments = Consumer $ \c -> (Success c, [])
+
 toPathInfo :: (PathInfo u) => u -> String
 toPathInfo = encodePathInfo . toPathSegments
 
+-- should this fail if not all the input was consumed?
 fromPathInfo :: (PathInfo u) => String -> Failing u
-fromPathInfo = fromPathSegments . decodePathInfo
+fromPathInfo pi = fst $ runConsumer (decodePathInfo pi) fromPathSegments 
