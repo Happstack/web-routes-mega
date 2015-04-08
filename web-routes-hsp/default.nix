36fe6f10b31e3b8f47fc7bf08dd73eacc8b7b22c
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-07 22:33:44 -0500

    added more .nix files

---
+++ web-routes-hsp/default.nix
@@ -0,0 +1,9 @@
+{ mkDerivation, base, hsp, stdenv, text, web-routes }:
+mkDerivation {
+  pname = "web-routes-hsp";
+  version = "0.24.5";
+  src = ./.;
+  buildDepends = [ base hsp text web-routes ];
+  description = "Adds XMLGenerator instance for RouteT monad";
+  license = stdenv.lib.licenses.bsd3;
+}
